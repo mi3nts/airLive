7a3354690289725b24c32fb7811701f715d39c5f
teamlary <you@example.com>
2020-02-24 10:07:08 -0600

    live Update

---
+++ firmware/LiveOutput/MD_0000.m
@@ -0,0 +1,29 @@
+
+ 
+MD_001e06305a12_Definitions
+
+MD_001e06305a12_Loader
+
+MD_001e06323a06_Definitions
+
+MD_001e06323a06_Loader
+
+
+while(true)
+    syncFrom
+        try
+        MD_001e06305a12_Definitions
+        MD_001e06305a12_main
+        catch
+           warning('Redoing Loop');
+        end
+        
+        try
+        MD_001e06323a06_Definitions
+        MD_001e06323a06_main
+        catch
+           warning('Redoing Loop');
+        end
+
+    syncTo 
+end
